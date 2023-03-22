94094105d0952172bbb9270980e79f13c0a0c913
Hallowjack1 <114891763+Hallowjack1@users.noreply.github.com>
2023-03-22 10:06:58 +0800

    Add files via upload

--- Delicioso/SelectID.php
+++ Delicioso/SelectID.php
@@ -1,7 +1,7 @@
 <?php
     require("config.php");
     $code = $_REQUEST['code'];
-    $sql = "SELECT ID FROM customerorder WHERE Booktitle LIKE '%$code%'";
+    $sql = "SELECT ID FROM customerorder WHERE CustomerName LIKE '%$code%'";
 
 //count how many people
 //$sql = "SELECT COUNT(*) FROM tbl_people WHERE Fullname LIKE '%$code\"";
